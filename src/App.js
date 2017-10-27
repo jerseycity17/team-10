b4838dda1930e2becc6cb9a43972aa893c67f021
coldGingerAle <hui.lin69@qmail.cuny.edu>
2017-10-27 19:57:21 -0400

    First commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// in src/App.js
+import React from 'react';
+import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+
+import { PostList } from './posts';
+
+const App = () => (
+    <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} />
+    </Admin>
+);
+
+export default App;
