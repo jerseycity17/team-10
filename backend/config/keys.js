a2e23c271ac8fbdef83c8212fd9e3f76f8ff028b
syedchowdhury426 <syedchowdhury426@live.com>
2017-10-27 20:39:33 -0400

    backend files uploaded

---
+++ backend/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:  '659259416229-u7vr3p4m9o0kmu7jcqvulq0db2pru2ki.apps.googleusercontent.com',
+  googleClientSecret: '3jBHLgUemX79xS8xg4AMa8zw',
+  mongoURI: 'mongodb://root:password@ds237815.mlab.com:37815/emaily-dev',
+  cookieKey: 'asdfghjkiouytredsxfcgvhbjkiuytrrd'
+};
