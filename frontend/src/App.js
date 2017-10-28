d8ffbb529c066a609e572f814b5a45c56ee66ebb
Michel Chemas <mchemas@Michels-MacBook-Pro.local>
2017-10-28 02:02:52 -0400

    connecting rest client to server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,10 @@ import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import { PostList, PostShow } from './posts';
 import { FamilyList, FamilyShow } from './family';
 import restClient from './restClient'
-import { PostList } from './posts';
 
 
 const App = () => (
-    <Admin title="Family Promise Admin" authClient = {authClient} restClient={restClient}>
+    <Admin title="Family Promise Admin" authClient = {authClient} restClient={restClient('http://localhost:8080')}>
         <Resource name="posts" list={PostList} show={PostShow} />
         <Resource name="family" list={FamilyList} show={FamilyShow} />
     </Admin>
