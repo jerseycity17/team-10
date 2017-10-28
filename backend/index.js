0346567258de3c8024bef02a9b43bcbd7d0ffb59
kr3110 <n.nkr3110@gmail.com>
2017-10-28 03:26:48 -0400

    cors control

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,26 @@ const Bill = require('./models/Bills');
 const worker = require('./models/CaseWorker');
 const family = require('./models/Family');
 const text = require('./models/Text')
-const familyRoute= require('./routes/family');
+const familyRoute= require('./routes/family')b/////;
 const smsresponse = twilio.twiml.MessagingResponse;
 
 require('./services/passport')(passport);
 
 mongoose.connect(keys.mongoURI);
-
+//cors control
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(session({
     secret: keys.cookieKey, // session secret
