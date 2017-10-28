3b9c56b761f970af4c116e59cad54b6f99a29828
kr3110 <n.nkr3110@gmail.com>
2017-10-27 22:23:00 -0400

    twilio webhook

--- backend/index.js
+++ backend/index.js
@@ -3,25 +3,34 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const twilio = require('twilio');
 const passport = require('passport');
-
+const smsresponse= twilio.twiml.MessagingResponse;
+const morgan = require('morgan');
+const bodyParser= require('body-parser');
 const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
-
+//require('./models/User');
+//require('./services/passport');
+const app = express();
 mongoose.connect(keys.mongoURI);
-
-
-//twilio set up
 var twilioClient= new twilio(keys.TwilioSID,keys.Twiliotoken);
 
-twilioClient.messages.create({
-    body: 'Hello from Node',
-    to: '+19294351864',  // Text this number
-    from: keys.number
-})
-.then((message) => console.log(message.sid));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.post('/sms',(req,res)=>{
+  const number = req.from;
+  const message = req.body;
+  
+});
+//twilio set up
 
-const app = express();
+//
+// twilioClient.messages.create({
+//     body: 'Hello from Node',
+//     to: '+19294351864',  // Text this number
+//     from: keys.number
+// })
+// .then((message) => console.log(message.sid));
+
 
 app.use(
   cookieSession({
@@ -35,5 +44,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
