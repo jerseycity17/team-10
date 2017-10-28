058830dfef56634bbdf51ea8297a07a65fccfa9c
coldGingerAle <hui.lin69@qmail.cuny.edu>
2017-10-28 00:29:41 -0400

    Changed family icon.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,12 @@ import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import { PostList, PostShow } from './posts';
 import { FamilyList, FamilyShow } from './family';
 
+import FamilyIcon from 'material-ui/svg-icons/social/group';
+
 const App = () => (
     <Admin title="Family Promise Admin" authClient = {authClient} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
         <Resource name="posts" list={PostList} show={PostShow} />
-        <Resource name="family" list={FamilyList} show={FamilyShow} />
+        <Resource name="family" list={FamilyList} show={FamilyShow} icon={FamilyIcon}/>
     </Admin>
 );
 
