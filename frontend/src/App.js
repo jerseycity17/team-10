2e54afd6377ff5b55dfaea9b60515709bb221731
Michel Chemas <mchemas@Michels-MacBook-Pro.local>
2017-10-28 00:15:52 -0400

    restClient dummy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 // in src/App.js
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-
+import restClient from './restClient'
 import { PostList } from './posts';
 
 const App = () => (
-    <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+    <Admin restClient={restClient}>
         <Resource name="posts" list={PostList} />
     </Admin>
 );
